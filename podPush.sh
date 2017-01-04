cae1ada10a60e5e49ca74f7da671420a6692cabc
梁慧聪 <chuckliang@kugou.net>
2017-01-04 19:49:26 +0800

    pod 0.1.9

---
+++ podPush.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# 例子 sh podPush.sh ChuckTableView 0.0.1
+#if [ -z $1 ]
+#then
+#echo "项目名："$1"不能为空"
+#exit 0 
+#fi
+#if [ -z $2 ]
+#then
+#echo "版本号："$2"不能为空"
+#exit 0 
+#fi
+#cd $1
+list=`find *.podspec`
+podspec=${list[0]}
+version=`sed -n -e '/s.version      = "/p' ${podspec} | awk '{print $3}' | sed 's/"//g'`
+#ver=$(${version}+1)
+#sed -n 's/${version}//g'
+echo '操作'${podspec}', 版本号：'$version
+
+git add -A
+git commit -m "pod "$version
+git push origin master
+git tag -d ''$version
+git tag ''$version
+git push --tags -f
+pod trunk push ${project} --allow-warnings
+#cd ..
