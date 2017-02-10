0156fafd5b1e126d80871ea2d6eb91804168a91e
梁慧聪 <chuckliang@kugou.net>
2017-02-10 18:58:16 +0800

    pod 0.2.4

--- podPush.sh
+++ podPush.sh
@@ -15,20 +15,29 @@ list=`find *.podspec`
 podspec=${list[0]}
 version=`sed -n -e '/s.version      = "/p' ${podspec} | awk '{print $3}' | sed 's/"//g'`
 ver=${version//./}
+ver=`expr $ver + 0`
 ver=$((ver+1))
-version=${ver:0:1}'.'${ver:1:1}'.'${ver:2:1}
+if [ $ver -gt 99 ]
+then
+	ver=${ver:0:1}'.'${ver:1:1}'.'${ver:2:1}
+else
+	if [ $ver -gt 9 ]
+	then
+		ver='0.'${ver:0:1}'.'${ver:1:1}
+	else
+		ver='0.0.'${ver}
+	fi
+fi
+sed -i '' 's/'$version'/'$ver'/' ${podspec}
+echo '操作'${podspec}', 旧版本号：'$version',新版本：'$ver
 
-#ver=$(${version}+1)
-#sed -n 's/${version}//g'
-echo '操作'${podspec}', 旧版本号：'$version'
-:'
 git pull
 git add -A
-git commit -m "pod "$version
+git commit -m "pod "$ver
 git push origin master
-git tag -d ''$version
-git tag ''$version
+git tag -d $version
+git tag $version
 git push --tags -f
 pod trunk push ${project} --allow-warnings
-'
+
 #cd ..
